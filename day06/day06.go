00e2ba33a97cd3e6eb7c5b944f137b36fe649f91
Juliane Holzt <juliane@holzt.de>
2023-12-06 10:13:36 +0100

    refactor: day06 math up

--- day06/day06.go
+++ day06/day06.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"math"
+
 	"github.com/julijane/advent-of-code-2023/aoc"
 )
 
 func calcVariants(raceTime, toBeat int) int {
-	for buttonTime := 0; buttonTime < raceTime; buttonTime++ {
-		if (raceTime-buttonTime)*buttonTime > toBeat {
-			return raceTime - 2*buttonTime + 1
-		}
-	}
+	rf := float64(raceTime)
+	tbf := float64(toBeat)
 
-	return 0
+	sq := math.Sqrt(rf*rf - 4*tbf)
+	return int(
+		math.Ceil((rf+sq)/2) - math.Floor((rf-sq)/2) - 1)
+
+	// for buttonTime := 0; buttonTime < raceTime; buttonTime++ {
+	// 	if (raceTime-buttonTime)*buttonTime > toBeat {
+	// 		return raceTime - 2*buttonTime + 1
+	// 	}
+	// }
+	//
+	// return 0
 }
 
 func getSingleNumber(line string) int {
